2af1faa4b936995ecfb952b5653f9f727eeeb90a
Calum <calumcmccall@gmail.com>
2022-08-22 11:57:19 +0100

    Stepper structure complete with dummy data

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -5,6 +5,7 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 const Rules = () => {
 
@@ -12,9 +13,9 @@ const Rules = () => {
 
     const steps = ['Step One', 'Step Two', 'Step Three'];
 
-    // const rulesStepper = () => {
-    //     const [activeStep, setActiveStep] = useState(0);
-    // }
+    const description = ['Description of first step', 'Description of second step', 'Description of third step'];
+
+    const images = ["https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg", "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg", "https://media.4-paws.org/e/8/2/7/e82789b9dc8a986d3b61c0aa7610affeecb93933/VIER%20PFOTEN_2015-04-27_010-1927x1333.jpg"];
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -33,12 +34,12 @@ const Rules = () => {
             <div className="rules display" >
                 <Box sx={{width: '100%'}}>
                     <Stepper activeStep={activeStep}>
-                        {steps.map((label, index) => {
+                        {steps.map((step, index) => {
                             const stepProps = {};
                             const labelProps = {};
                             return (
-                                <Step key={label} {...stepProps}>
-                                    <StepLabel {...labelProps}>{label}</StepLabel>
+                                <Step key={index} {...stepProps}>
+                                    <StepLabel {...labelProps}>{step}</StepLabel>
                                 </Step>
                             );
                         })}
@@ -50,12 +51,14 @@ const Rules = () => {
                             </Typography>
                             <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
                                 <Box sx={{flex: '1 1 auto'}} />
+                                <Link to="/"><Button>Return to home</Button></Link>
                                 <Button onClick={handleReset}>Reset</Button>
                             </Box>
                         </>
                     ) : (
                         <>
-                            <Typography sx={{ mt: 2, mb: 1}}>Step {activeStep + 1}</Typography>
+                            <Typography sx={{ mt: 2, mb: 1}}>{description[activeStep]}</Typography>
+                            <img src={images[activeStep]} />
                             <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
                                 <Button 
                                     color="inherit"
