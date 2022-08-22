dee926580ed63e35945c8e046b6881bb21006d42
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-08-22 13:38:05 +0100

    split hand works as promised, hopefully

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -12,6 +12,7 @@ const Game = () => {
     const [playerHand, setPlayerHand] = useState([])
     const [splitHand, setSplitHand] = useState([])
     const [palyerStand, setPlayerStand] = useState(false)
+    const [splitStand, setSplitSantd] = useState(false)
     const [player, setPlayer] = useState({
         name: "",
         wallet: "",
@@ -20,6 +21,7 @@ const Game = () => {
         background: ""
     })
     const [winner, setWinner] = useState('')
+    const [splitWinner, setSplitWinner] = useState('')
 
     // Fetch all cards
     useEffect(() => {
@@ -34,6 +36,13 @@ const Game = () => {
             setPlayerStand(true)
         }}, [playerHand])
 
+    //auto stops if split hand has more than 21 points
+    useEffect(() => {
+        if (getHandValue(playerHand) > 21) {
+            setSplitSantd(true)
+        }}, [splitHand])
+
+        
     // Fetches the starting hands
     const handleClick = () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
@@ -41,16 +50,21 @@ const Game = () => {
             .then(data => {
                 setDealersHand([data.cards[0], data.cards[1]])
                 setPlayerHand([data.cards[2], data.cards[3]])
+                setSplitHand([])
                 setPlayerStand(false)
+                setSplitSantd(false)
                 splitButton()
             })
+            if (playerHand[0].value === playerHand[1].value && playerHand[0].value ==="Ace") {
+                split()
         }
-        
+        }
         useEffect(() => {
         setWinner(blackjackGameLogic(dealersHand, playerHand))
+        setSplitWinner(blackjackGameLogic(dealersHand, splitHand))
     }, [playerHand])
 
-    
+    // separets the dealer's cards to show
     const dealerCardsNodes = dealersHand.map((card, index) => {
         return (
             <div className='hand' key={index}>
@@ -58,7 +72,8 @@ const Game = () => {
             </div>
         )
     })
-
+    
+    // separets the player's cards to show
     const playerCardsNodes = playerHand.map((card, index) => {
         return (
             <Draggable>
@@ -67,7 +82,7 @@ const Game = () => {
         )
     })
     
-    
+    // gives player anouther card, makes player stop if the card gets the points over 21
     const handleHitClick = () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
         .then(res => res.json())
@@ -80,11 +95,7 @@ const Game = () => {
         } 
     }
     
-    // useEffect (() => {
-    
-    // }, [palyerStand])
-
-    
+    // dealer getting another card, happens at the end of the game when player stands
     const dealerHit = () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
         .then(res => res.json())
@@ -94,6 +105,7 @@ const Game = () => {
         })
     }
     
+    // set player stand, allows dealer to play after
     const handleStandClick = () => {
         setPlayerStand(true);
         if (getHandValue(dealersHand) < 17) {
@@ -101,6 +113,7 @@ const Game = () => {
         }
     }
 
+    // if there are two of the same value allows player to split and deal with each separatly
     const split = () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
@@ -112,6 +125,7 @@ const Game = () => {
         })
     }
     
+    // the split button
     const splitButton = () => {
         if (playerHand.length === 2) {
             if (playerHand[0].value === playerHand[1].value) {
@@ -121,6 +135,7 @@ const Game = () => {
     }
     
 
+    // shows the split hand
     const splitCardsNodes = splitHand.map((card, index) => {
         return (
             <Draggable>
@@ -129,6 +144,7 @@ const Game = () => {
         )
     })
 
+    // gives the split hand a card
     const handleSplitHit = () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
         .then(res => res.json())
@@ -161,7 +177,7 @@ const Game = () => {
 
                 {playerHand.length ? <button onClick={handleClick}>{palyerStand ? "Play again" : "Forfit" } </button> : <button onClick={handleClick}>Draw card</button>}
 
-                {palyerStand ? <p>Play another round?</p> : <>
+                {palyerStand && splitStand ? <p>Play another round?</p> : <>
                 {playerHand.length ? <button onClick={handleHitClick}>Hit</button> : <></> }
                 {splitHand.length ? <button onClick={handleSplitHit}>Hit second hand</button> : <></>}
                 {playerHand.length ? <button onClick={handleStandClick}>Stand</button> : <></> }
@@ -171,7 +187,8 @@ const Game = () => {
 
 
                 {palyerStand ?<p>{winner}</p>:<p>{getHandValue(playerHand)}</p>}
-                {splitHand.length ? <p>{getHandValue(splitHand)}</p> : <></>}
+                {palyerStand && !splitHand.length ?<p>{splitWinner}</p>:<></>}
+                {splitHand.length? getHandValue(splitHand) : <></>}
             </div>
         </>
     )
