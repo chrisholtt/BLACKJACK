7bca26fe9d21196dcd9098f300fb3ca98f5eb5ba
Chris <chrisphilholt@gmail.com>
2022-08-24 22:35:14 +0100

    added steps

--- client/src/components/Rules.js
+++ client/src/components/Rules.js
@@ -13,9 +13,9 @@ const Rules = () => {
 
     const steps = ['Place Wager', 'Draw Cards', 'Players Hand', 'Dealers Hand'];
 
-    const description = ['Description of first step', 'Description of second step', 'Description of third step', 'Description of fourth step'];
+    const description = ['Set your wager', 'Click draw cards to start the game', 'The bottom row is the players cards', 'The top row is the dealers cards'];
 
-    const images = ["https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg", "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg", "https://media.4-paws.org/e/8/2/7/e82789b9dc8a986d3b61c0aa7610affeecb93933/VIER%20PFOTEN_2015-04-27_010-1927x1333.jpg"];
+    const images = ["/static/wager.gif", "/static/draw.gif", "/static/players-hand.png", "/static/dealers-cards.png"];
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -32,7 +32,7 @@ const Rules = () => {
     return (
         <div className="wrapper">
             <div className="rules display" >
-                <Box sx={{width: '100%'}}>
+                <Box sx={{ width: '100%' }}>
                     <Stepper activeStep={activeStep}>
                         {steps.map((step, index) => {
                             const stepProps = {};
@@ -46,35 +46,35 @@ const Rules = () => {
                     </Stepper>
                     {activeStep === steps.length ? (
                         <>
-                            <Typography sx={{mt: 2, mb: 1}}>
+                            <Typography sx={{ mt: 2, mb: 1 }}>
                                 All Steps completed - you done
                             </Typography>
-                            <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
-                                <Box sx={{flex: '1 1 auto'}} />
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Box sx={{ flex: '1 1 auto' }} />
                                 <Link to="/"><Button>Return to home</Button></Link>
                                 <Button onClick={handleReset}>Reset</Button>
                             </Box>
                         </>
                     ) : (
                         <>
-                            <Typography sx={{ mt: 2, mb: 1}}>{description[activeStep]}</Typography>
+                            <Typography sx={{ mt: 2, mb: 1 }}>{description[activeStep]}</Typography>
                             <img src={images[activeStep]} />
-                            <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
-                                <Button 
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Button
                                     color="inherit"
                                     disabled={activeStep === 0}
                                     onClick={handleBack}
-                                    sx={{ mr: 1}}
+                                    sx={{ mr: 1 }}
                                 >
                                     Back
                                 </Button>
-                                <Box sx={{ flex: '1 1 auto'}} />
+                                <Box sx={{ flex: '1 1 auto' }} />
                                 <Button onClick={handleNext}>
                                     {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                                 </Button>
                             </Box>
                         </>
-                    ) }
+                    )}
                 </Box>
             </div>
         </div>
