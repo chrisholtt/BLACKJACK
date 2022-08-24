2cdb37694e83888222b6dc4d7885fa16a61cab2a
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-08-24 15:13:29 +0100

    removed two player option

--- client/src/containers/PlayerModes.js
+++ client/src/containers/PlayerModes.js
@@ -10,7 +10,6 @@ const PlayerModes = () => {
                 <h1>Pick a game mode:</h1>
                 <div className='play-modes-container'>
                     <Link to="/players1"> <div className="play-mode">1 Player</div> </Link>
-                    <Link to="/players2"><div className="play-mode">2 Players</div></Link>
                     <Link to="/rules"><div className="play-mode">Rules</div></Link>
                 </div>
             </div>
